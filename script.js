df88b5959920df876700201a1b8fdec2bf96c3ca
jwngx <pcjwngx@gmail.com>
2026-04-09 00:01:19 +0530

    code updated

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const input = document.getElementById('userInput');
 const submit = document.getElementById('submitBtn');
 const todoList = document.getElementById('todoList');
 
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    const text = input.value.trim();
-    if (text === "") return;
-    const formattedText = text.charAt(0).toUpperCase() + text.slice(1);
-    const li = document.createElement('li');
-    li.classList.add('li_items');
+function formatText(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
+
+function createCheckbox(span) {
     const checkbox = document.createElement('input');
-    checkbox.classList.add('checkbox');
     checkbox.type = 'checkbox';
-    const span = document.createElement('span');
-    span.innerText = formattedText;
-    span.classList.add('todoTaskText')
+    checkbox.classList.add('checkbox');
 
     checkbox.addEventListener('change', () => {
         span.classList.toggle('done', checkbox.checked);
     });
+    return checkbox;
+};
+
+function deleteItem(li) {
     const delBtn = document.createElement('button');
     delBtn.classList.add('del');
     delBtn.innerText = 'Delete';
@@ -26,10 +26,28 @@ submit.addEventListener('click', (e) => {
     delBtn.addEventListener('click', () => {
         li.remove();
     });
+    return delBtn;
+}
 
+function createItem(text) {
+    const li = document.createElement('li');
+    li.classList.add('li_items');
+    const span = document.createElement('span');
+    span.innerText = formatText(text);
+    span.classList.add('todoTaskText')
+    const checkbox = createCheckbox(span)
+    const delbtn = deleteItem(li);
     li.appendChild(checkbox);
     li.appendChild(span);
-    li.appendChild(delBtn);
-    todoList.appendChild(li);
+    li.appendChild(delbtn);
+    return li;
+}
+function handleTodo(e) {
+    e.preventDefault();
+    const text = input.value.trim();
+    if (text === "") return;
+    const todoItem = createItem(text);
+    todoList.appendChild(todoItem);
     input.value = '';
-});
+}
+submit.addEventListener('click', handleTodo)
